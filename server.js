d17042e463ddb74f43bea1c3d2bd64190eab9e46
ab-x-cd <nathseitz7@gmail.com>
2025-03-11 16:42:40 +0100

    update portfolio 11/03/2025

--- server.js
+++ server.js
@@ -3,30 +3,28 @@ const app = express();
 const port = 3070;
 const path = require("path");
 
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
   });
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'assets')));
-
 app.get("/animscss", (req, res) => {
-    res.sendFile(path.join(__dirname + '/projets_css/accueilcss.html'));
-  });
+  res.sendFile(path.join(__dirname, "projets_css", "accueilcss.html"));  });
 
 app.get("/animbounce", (req, res) => {
-    res.sendFile(path.join(__dirname + '/projets_css/bounce/animrebond.html'));
-  });
+  res.sendFile(path.join(__dirname, "projets_css", "bounce", "animrebond.html"));  });
  
 app.get("/solar", (req, res) => {
-    res.sendFile(path.join(__dirname + '/projets_css/solar/solar.html'));
+  res.sendFile(path.join(__dirname, "projets_css", "solar", "solar.html"));  });
+
+
+  app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "errors", "404.html"));
   });
-
-
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, '/errors/404.html'));
-});
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -35,5 +33,5 @@ app.use((err, req, res, next) => {
 
 
 app.listen(port, () => {
-    console.log(`Server is UP on port ${port}`);
-});
+  console.log(`🚀 Serveur en ligne sur http://localhost:${port}`);
+  });
